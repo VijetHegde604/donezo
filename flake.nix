defbc3ea2622b97ff6b938269b2a086b6ada7fed
VijetHegde604 <vijethegde604@gmail.com>
2026-09-24 23:17:08 +0530

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Development environment for Donezo - a Go CLI todo app";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              go
+              gopls
+              delve
+              golangci-lint
+            ];
+          };
+        }
+      );
+    };
+}
