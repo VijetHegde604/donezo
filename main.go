bd16fc642999b2cb83fdf73216291b4565218aba
VijetHegde604 <vijethegde604@gmail.com>
2026-09-25 22:38:23 +0530

    implemented delete,status-update and reset, Completed core features

--- main.go
+++ main.go
@@ -5,87 +5,255 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/google/uuid"
 )
 
 type Task struct {
-	ID          int    `json:"id"`
+	ID          string `json:"id"`
 	Description string `json:"description"`
 	Completed   bool   `json:"completed"`
 }
 
-func AddTask(id int, desc string) (Task, int) {
-	newTask := Task{Completed: false, Description: desc, ID: id}
-	return newTask, id + 1
+// Add a task containing an uuid
+// and completion status
+func AddTask(desc string) (Task, error) {
+	uid, err := uuid.NewV7()
+	if err != nil {
+		return Task{}, err
+	}
+
+	newTask := Task{
+		Completed:   false,
+		Description: desc,
+		ID:          uid.String(),
+	}
+
+	return newTask, nil
 }
 
-func listTask(tasks []Task) {
-	for _, task := range tasks {
-		fmt.Printf("Id: %d, Desc: %s, Status: %t\n", task.ID, task.Description, task.Completed)
+// List tasks as a formatted list of strings
+func ListTask(tasks []Task) {
+	status := ""
+	for index, task := range tasks {
+		if task.Completed {
+			status = "completed"
+		} else {
+			status = "pending"
+		}
+
+		fmt.Printf("%d. %s, Status: %s\n",
+			index+1,
+			task.Description,
+			status,
+		)
 	}
 }
 
+func DeleteTask(ids []int, tasks *[]Task) error {
+	// Delete from highest index to lowest so initial indexes
+	// don't shift before deleting them.
+	sort.Sort(sort.Reverse(sort.IntSlice(ids)))
+
+	for _, id := range ids {
+		if id < 1 {
+			return errors.New("task ID must be greater than 0")
+		}
+
+		found := false
+
+		for index := range *tasks {
+			if index+1 == id {
+				*tasks = append((*tasks)[:index], (*tasks)[index+1:]...)
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			return fmt.Errorf("task %d not found", id)
+		}
+	}
+
+	return nil
+}
+
+func CompleteTask(ids []int, tasks *[]Task) error {
+	for _, id := range ids {
+		if id < 1 {
+			return errors.New("task ID must be greater than 0")
+		}
+
+		found := false
+
+		for index := range *tasks {
+			if index+1 == id {
+				(*tasks)[index].Completed = true
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			return fmt.Errorf("task %d not found", id)
+		}
+	}
+
+	return nil
+}
+
+func Reset(tasks *[]Task) error {
+	*tasks = []Task{}
+	return nil
+}
+
 func main() {
-	fmt.Println("Donezo")
 	tasks := []Task{}
-	nextID := 0
 	filename := "tasks.json"
 	userArgs := os.Args[1:]
 
-	// Check if the file exist first
+	// Check if the file exists first
 	_, err := os.Stat(filename)
+
 	if errors.Is(err, os.ErrNotExist) {
-		// First run: File does not exist
-		// File will be created by os.OpenFile while saving tasks
+		// First run: file does not exist.
+		// File will be created by os.OpenFile while saving tasks.
 	} else if err != nil {
-		fmt.Printf("Error checking file: %s\nErr: %v", filename, err)
+		fmt.Printf("Error checking file: %s\nErr: %v\n", filename, err)
+		return
 	} else {
-		// Read the file and store the data to tasks
+		// Read the file and store the data in tasks
 		fileBytes, err := os.ReadFile(filename)
 		if err != nil {
-			fmt.Printf("Something went wrong!\nErr: %v", err)
+			fmt.Printf("Something went wrong!\nErr: %v\n", err)
 			return
 		}
 
 		err = json.Unmarshal(fileBytes, &tasks)
 		if err != nil {
-			fmt.Printf("Cannot decode json file: %s\nErr: %v", filename, err)
+			fmt.Printf("Cannot decode json file: %s\nErr: %v\n", filename, err)
 			return
 		}
 	}
 
-	// Set the nextID to the highest existing ID + 1
-	if len(tasks) > 0 {
-		nextID = tasks[len(tasks)-1].ID + 1
+	if len(userArgs) < 1 {
+		fmt.Println("Usage: donezo <add|list|remove>")
+		return
 	}
 
 	operation := userArgs[0]
+
 	switch operation {
+
 	case "add":
-		newTask, id := AddTask(nextID, userArgs[1])
-		nextID = id
+		if len(userArgs) < 2 {
+			fmt.Println("Usage: donezo add <description>")
+			return
+		}
+
+		// Join all arguments after "add" into one description.
+		description := strings.Join(userArgs[1:], " ")
+
+		newTask, err := AddTask(description)
+		if err != nil {
+			fmt.Printf("Something went wrong!\nErr: %v\n", err)
+			return
+		}
+
 		tasks = append(tasks, newTask)
-		listTask(tasks)
-	case "list":
-		listTask(tasks)
+
+	case "list", "ls":
+		if len(tasks) < 1 {
+			fmt.Println("No tasks!")
+			return
+		}
+
+		ListTask(tasks)
+
+	case "remove", "rm":
+		if len(userArgs) < 2 {
+			fmt.Println("Usage: donezo remove <id,id,id>")
+			return
+		}
+
+		stringIDs := strings.Split(userArgs[1], ",")
+		taskIDs := []int{}
+
+		for _, task := range stringIDs {
+			tid, err := strconv.Atoi(task)
+			if err != nil {
+				fmt.Printf("Invalid task ID %q\n", task)
+				return
+			}
+
+			taskIDs = append(taskIDs, tid)
+		}
+
+		err = DeleteTask(taskIDs, &tasks)
+		if err != nil {
+			fmt.Printf("Something went wrong!\nErr: %v\n", err)
+			return
+		}
+
+	case "done":
+		if len(userArgs) < 2 {
+			fmt.Println("Usage: donezo done <id,id,id>")
+			return
+		}
+
+		stringIDs := strings.Split(userArgs[1], ",")
+		taskIDs := []int{}
+
+		for _, task := range stringIDs {
+			tid, err := strconv.Atoi(task)
+			if err != nil {
+				fmt.Printf("Invalid task ID %q\n", task)
+				return
+			}
+
+			taskIDs = append(taskIDs, tid)
+		}
+
+		err = CompleteTask(taskIDs, &tasks)
+		if err != nil {
+			fmt.Printf("Something went wrong!\nErr: %v\n", err)
+			return
+		}
+
+	case "reset", "rs":
+		err = Reset(&tasks)
+		if err != nil {
+			fmt.Printf("Something went wrong!\nErr: %v\n", err)
+			return
+		}
+
 	}
 
 	data, err := json.Marshal(tasks)
 	if err != nil {
-		fmt.Println("Something went wrong! Err: ", err)
+		fmt.Printf("Something went wrong!\nErr: %v\n", err)
 		return
 	}
 
 	// Write the data to the file
-	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0660)
+	f, err := os.OpenFile(
+		filename,
+		os.O_WRONLY|os.O_TRUNC|os.O_CREATE,
+		0660,
+	)
 	if err != nil {
-		fmt.Printf("Error opening file: %s\n Err: %v", filename, err)
+		fmt.Printf("Error opening file: %s\nErr: %v\n", filename, err)
 		return
 	}
 
+	defer f.Close()
+
 	_, err = f.Write(data)
 	if err != nil {
-		fmt.Printf("Cannot write to file: %s\nErr: %v", filename, err)
+		fmt.Printf("Cannot write to file: %s\nErr: %v\n", filename, err)
+		return
 	}
-
-	defer f.Close()
 }
